6cdad1e6d015ca0efeca907416dbcc5c2b8cacd5
nairenmufia <nairenmufia@gmail.com>
2026-07-11 12:14:00 +0700

    Add HeaderSitus

--- components/HeaderSitus.tsx
+++ components/HeaderSitus.tsx
@@ -1,7 +1,3 @@
 export function HeaderSitus() {
-  return (
-    <header className="p-4 bg-black text-white text-center text-2xl font-bold">
-      100PERAK
-    </header>
-  )
+  return <header className="p-4 bg-black text-white text-center text-2xl font-bold">100PERAK</header>
 }
