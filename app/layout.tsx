c8676bb510f883f9d96886113308694fbc5c8dd8
nairenmufia <nairenmufia@gmail.com>
2026-07-11 19:50:05 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
-export const metadata = {
-  title: '100 Perak',
-  description: 'Aplikasi Antrian 100 Perak',
-}
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="id">
+    <html lang="en">
       <body>{children}</body>
     </html>
   )
