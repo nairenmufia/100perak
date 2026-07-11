58925cdd437a604b5404321cc780ecbe4dc1768c
nairenmufia <nairenmufia@gmail.com>
2026-07-11 10:30:25 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,15 @@
+import { HeaderSitus } from "@/components/HeaderSitus"
+import { AplikasiAntrian } from "@/components/AplikasiAntrian"
+import { CaraBermain } from "@/components/CaraBermain"
+import { KakiSitus } from "@/components/KakiSitus"
 
+export default function Halaman() {
+  return (
+    <main className="min-h-screen bg-black">
+      <HeaderSitus />
+      <AplikasiAntrian />
+      <CaraBermain />
+      <KakiSitus />
+    </main>
+  )
+}
