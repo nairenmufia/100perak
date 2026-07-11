e330e185bbd1006b0c6d10ed34dc540f1125334e
nairenmufia <nairenmufia@gmail.com>
2026-07-11 19:20:46 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{padding: 20, textAlign: 'center'}}>
-      <h1>🔥 100 PERAK JALAN 🔥</h1>
-      <p>Kalau ini muncul berarti udah sukses</p>
-    </main>
-  )
+  return <h1>Halo 100perak</h1>
 }
