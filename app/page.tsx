26f70db356b50bb6c3322bd4f7b3002b7b070420
nairenmufia <nairenmufia@gmail.com>
2026-07-11 17:56:59 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: 20, textAlign: 'center'}}>
+      <h1>🔥 100 PERAK JALAN 🔥</h1>
+      <p>Kalau ini muncul berarti udah sukses</p>
+    </main>
+  )
+}
