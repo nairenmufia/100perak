2b198804099c63683216ae9a1e8b00d542d93edd
nairenmufia <nairenmufia@gmail.com>
2026-07-13 20:28:39 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <h1 style={{textAlign: 'center', marginTop: 100}}>100perak TEST UDAH MUNCUL 🔥</h1>
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1>100perak siap 🚀</h1>
+    </main>
+  )
 }
